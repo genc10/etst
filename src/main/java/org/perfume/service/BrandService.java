beb41e2199a533c0cb197be763f41edc18c0fa9c
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-28 18:28:19 +0400

    Brand Service updated

--- src/main/java/org/perfume/service/BrandService.java
+++ src/main/java/org/perfume/service/BrandService.java
@@ -8,4 +8,9 @@ import java.util.List;
 public interface BrandService {
     List<BrandResponse> searchBrands(String name);
     List<BrandResponse> getBrandsWithPerfumes();
+    BrandResponse create(BrandRequest request);
+    BrandResponse update(Long id, BrandRequest request);
+    void delete(Long id);
+    BrandResponse getById(Long id);
+    List<BrandResponse> getAll();
 }
